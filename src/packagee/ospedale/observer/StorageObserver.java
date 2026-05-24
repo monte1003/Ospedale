aed38fd031a168d3220bf10b9076a5a3a74bdc86
Juan Diego Vizcaíno <jd0i0i010i10i@gmail.com>
2026-05-24 14:15:57 -0500

    Deletion of Packages

--- src/packagee/ospedale/observer/StorageObserver.java
+++
@@ -1,9 +0,0 @@
-package packagee.ospedale.observer;
-
-/**
- * Permite reaccionar a cambios relevantes del almacenamiento compartido.
- */
-public interface StorageObserver {
-
-    void onStorageChanged(StorageEventType eventType);
-}
